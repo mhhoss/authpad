97245921f649c77342613b74ad79ae1923c99b9b
mhhoss <mhossza@gmail.com>
2025-10-15 19:51:21 +0330

    Add /auth prefix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.auth import routes
+from app.auth import routes as auth_router
 
 
 app = FastAPI()
-app.include_router(routes.router)
+app.include_router(auth_router, prefix="/auth")
 
