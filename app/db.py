e4a00d5d2a51564f6758760c7fc08d46ff78a14c
mhhoss <mhossza@gmail.com>
2025-10-16 17:34:38 +0330

    Refactor db: replace asycpg connection with databases.database

--- app/db.py
+++ app/db.py
@@ -1,15 +1,9 @@
-import asyncpg
 import os
+from databases import Database
 from dotenv import load_dotenv
 
-load_dotenv()
 
-async def get_conn():
-    return await asyncpg.connect(
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT"))
-    )
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+db = Database(DB_URL)
 
