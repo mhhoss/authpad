d68dbe26871817c5ed8b99701f64a226809ad0ee
mhhoss <mhossza@gmail.com>
2025-10-17 15:33:59 +0330

    Fix bcrypt crash by trimming password

--- app/utils/crypto.py
+++ app/utils/crypto.py
@@ -1,10 +1,12 @@
 from passlib.context import CryptContext
 
 
-pwd_context = CryptContext(schemes=["bcrypt"] , deprecated=["auto"])
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_pass(password: str) -> str:
-    return pwd_context.hash(password)
+    trimmed = password.encode("utf-8")[:72].decode("utf-8", errors="ignore")
+    return pwd_context.hash(trimmed)
 
 def verify_pass(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    trimmed = plain.encode("utf-8")[:72].decode("utf-8", errors="ignore")
+    return pwd_context.verify(trimmed, hashed)
