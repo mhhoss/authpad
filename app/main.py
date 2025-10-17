ef1908011769eba83a2ff121dc56964a09691185
mhhoss <mhossza@gmail.com>
2025-10-17 15:50:37 +0330

    Remove lifespan (for now)

--- app/main.py
+++ app/main.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
-from app.auth import routes as auth_router
-from app.db import db
-
-
-async def lifespan(app: FastAPI):
-    await db.connect()
-    yield
-    await db.disconnect()
+from app.routers.auth_routes import router as auth_router
+from app.routers.user_routes import router as user_router
 
 
 app = FastAPI(
     title="AuthPad API",
     description="Secure authentication API with JWT, FastAPI, and PostgreSQL",
-    version="0.1.0",
-    lifespan=lifespan
+    version="0.1.0"
     )
 
+
 app.include_router(auth_router, prefix="/auth")
+app.include_router(user_router, prefix="/user")
