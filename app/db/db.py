89947750a182056a2c1166709946684a42578474
mhhoss <mhossza@gmail.com>
2025-10-17 15:26:32 +0330

    Add check DB_URL before connecting

--- app/db/db.py
+++ app/db/db.py
@@ -9,6 +9,9 @@ DB_URL = os.getenv("DB_URL")
 
 
 async def get_conn():
+    DB_URL = os.getenv("DB_URL")
+    if not DB_URL:
+        raise RuntimeError("DB_URL not found in enviroment variables!")
     return await asyncpg.connect(DB_URL)
 
 
