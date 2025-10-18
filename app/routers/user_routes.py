f464e997a4371f36280e5bf7c7759105a4e98d45
mhhoss <mhossza@gmail.com>
2025-10-18 22:12:38 +0330

    Fix (user_routes): Update /me to return return UserRead schema

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends
 from app.core.security import get_current_user
-from app.schemas.schemas import UserOut
+from app.schemas.schemas import UserRead, UserRead
 
 
 router = APIRouter()
 
 # this will be used for user profile, dashboard, and settings
-@router.get("/me", response_model=UserOut)
-async def get_me(email: str = Depends(get_current_user)):  # registered user only
+@router.get("/me", response_model=UserRead)
+async def get_me(current_user = Depends(get_current_user)):  # registered user only
     """
     Return the current user's info using a valid JWT token.
     """
-    return UserOut(email=email)
+    return UserRead(**dict(current_user))
