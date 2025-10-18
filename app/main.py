8d472611ecf23fbac26fff66ae9f64cff31c22ae
mhhoss <mhossza@gmail.com>
2025-10-18 17:57:28 +0330

    Refactor (main): Improve structure and use lifespan

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, lifespan
 from app.routers.auth_routes import router as auth_router
 from app.routers.user_routes import router as user_router
+from app.db.db import init_pool, close_pool
+from contextlib import asynccontextmanager
 
 
 app = FastAPI(
     title="AuthPad API",
-    description="Secure authentication API with JWT, FastAPI, and PostgreSQL",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan=lifespan
     )
 
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(user_router, prefix="/user")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_pool()
+    yield
+    await close_pool()
