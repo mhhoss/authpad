75c9f85cc3559353ea35f03680832b37daace3e0
mhhoss <mhossza@gmail.com>
2025-10-19 21:28:09 +0330

    Docs (main): Add swagger tags for routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, lifespan
+from fastapi import FastAPI
 from app.routers.auth_routes import router as auth_router
 from app.routers.user_routes import router as user_router
 from app.db.db import init_pool, close_pool
 from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_pool()
+    yield
+    await close_pool()
 
 
 app = FastAPI(
@@ -13,12 +20,5 @@ app = FastAPI(
     )
 
 
-app.include_router(auth_router, prefix="/auth")
-app.include_router(user_router, prefix="/user")
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_pool()
-    yield
-    await close_pool()
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(user_router, prefix="/user", tags=["User Management"])
