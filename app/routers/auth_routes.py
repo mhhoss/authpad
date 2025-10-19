bd7f48690e5f2c243f2bf6b2986ea1d794e808bf
mhhoss <mhossza@gmail.com>
2025-10-19 21:18:33 +0330

    Refactor (auth_routers): Add access token expire time and HTTPException

--- app/routers/auth_routes.py
+++ app/routers/auth_routes.py
@@ -1,18 +1,17 @@
 from datetime import timedelta
-import uuid
 import asyncpg
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from app.db.db import get_conn
-from app.core.security import create_access_token, get_current_user
+from app.core.security import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_current_user
 from app.utils.crypto import hash_pass, verify_pass
-from app.schemas.schemas import UserCreate, UserRead, Token
+from app.schemas.schemas import TokenData, UserCreate, UserRead, Token
 
 
 router = APIRouter()
 
 
-@router.post("/register", response_model=UserRead)
+@router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
 async def register_user(user: UserCreate, conn: asyncpg.Connection = Depends(get_conn)):
     """
     Register a new user with email and password.
@@ -26,23 +25,22 @@ async def register_user(user: UserCreate, conn: asyncpg.Connection = Depends(get
     """
     existing = await conn.fetchrow("SELECT 1 FROM users WHERE email = $1", user.email)
     # check if the email is already registered
-
     if existing:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The email already registered!")
-    
-    user_id = str(uuid.uuid4())
+
     hashed_password = hash_pass(user.password)
 
-    # ایجاد رکورد
-    await conn.execute(
-        "INSERT INTO users (id, email, hashed_pass, is_verified) VALUES ($1, $2, $3, $4)", user_id, user.email, hashed_password, True
+    # ایجاد رکورد و خواندن اطلاعات و سپس برگرداندن آن
+    row = await conn.fetchrow(
+        """
+        INSERT INTO users (email, hashed_pass, is_verified) VALUES ($1, $2, $3)
+        RETURNING id, email, is_verified, created_at
+        """,
+        user.email, hashed_password, True
     )
 
-    # خواندن اطلاعات و نهایتا برگرداندن آن به کلاینت
-    row = await conn.fetchrow(
-        "SELECT id, email, is_verified, created_at FROM users WHERE id = $1",
-        user_id
-    )
+    if not row:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="User registration failed")
 
     return UserRead(**row)
 
@@ -66,7 +64,7 @@ async def token(
 
     # گرفتن اطلاعات کاربر از دیتابیس
     row = await conn.fetchrow(
-        "SELECT email, hashed_pass FROM users WHERE email = $1",
+        "SELECT id, email, hashed_pass, is_verified FROM users WHERE email = $1",
         form_data.username
     )
 
@@ -78,13 +76,12 @@ async def token(
         )
 
     # ساخت توکن JWT
-    access_token_expires = timedelta(minutes=60)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     token = create_access_token({"sub": row["email"]}, expires_delta=access_token_expires)
 
-    return Token(access_token=token)
-
+    return {"access_token": token, "token_type": "bearer", "expires_in": int(access_token_expires.total_seconds())}
 
 # for testing only
 @router.get("/protected")
-async def protected_area(email: str = Depends(get_current_user)):
-    return UserRead(email=email)
+async def protected_area(user: UserRead = Depends(get_current_user)):
+    return UserRead(user)
