31b9acee06a97c85d291d9dacb5dcdd16e64d822
mhhoss <mhossza@gmail.com>
2025-10-22 17:48:27 +0330

    Test (test_security): JWT creation, exp, decode errors

---
+++ tests/test_security.py
@@ -0,0 +1,58 @@
+import uuid
+import pytest
+from datetime import timedelta
+from app.core.security import ALGORITHM, SECRET_KEY, create_access_token
+from jose import ExpiredSignatureError, JWTError, jwt
+
+
+
+def test_create_access_token_valid():
+    data = {"sub": "user@gmail.com"}
+    expires = timedelta(minutes=20)
+
+    token = create_access_token(data=data, expires_delta=expires)
+
+    assert isinstance(token, str)
+
+    # شکافتن رشته برای استخراج اطلاعات و بررسی مقادیر -> sub & exp
+    decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    assert decoded["sub"] == "user@gmail.com"
+    assert "exp" in decoded
+
+    
+    jti = decoded.get("jti")
+    assert jti is not None
+    assert isinstance(jti, str)  # UUID
+
+    uuid_obj = uuid.UUID(jti)
+    assert str(uuid_obj) == jti
+
+
+def test_token_missing_sub():
+    data = {}  # بدون sub
+    token = create_access_token(data=data)
+
+    decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    assert "sub" not in decoded
+
+
+def test_expired_token_should_fail():
+    data = {"Sub": "expired_user"}
+    expires = timedelta(seconds=-1)  # منقضی شده
+    token = create_access_token(data=data, expires_delta=expires)
+
+    with pytest.raises(ExpiredSignatureError):
+        jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+
+def test_decode_with_wrong_algorithm_and_secret():
+    data = {"sub": "user@gmail.com"}
+    token = jwt.encode(data, SECRET_KEY, algorithm="HS256")  # الگوریتم درست
+
+    with pytest.raises(JWTError):
+        # الگوریتم اشتباه
+        jwt.decode(token, SECRET_KEY, algorithms=["RS256"])
+
+    with pytest.raises(JWTError):
+        jwt.decode(token, "wrong-secret", algorithms=[ALGORITHM])
