b852a85cc37e7b65f4eab45b6cc715e8a4eea2df
mhhoss <mhossza@gmail.com>
2025-10-27 23:44:52 +0330

    Initial commit: add config file

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Settings:
+    def __init__(self):
+        # Database
+        self.DB_URL = os.getenv("DATABASE_URL")
+            
+        # JWT Authentication
+        self.SECRET_KEY = os.getenv("SECRET_KEY")
+        self.ALGORITHM = os.getenv("ALGORITHM", "HS256")
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
+
+
+
+
+settings = Settings()
